8d7dc165c657af3023cc272ee245e08a3a2c0f77
solnce52004 <solnce52004@yandex.ru>
2021-09-12 04:10:11 +0300

    <1> add manifest and some correct in readme

---
+++ src/main/java/dev/example/App.java
@@ -0,0 +1,16 @@
+package dev.example;
+
+public class App {
+
+    public static final String HELLO_WORLD = "Hello World!";
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return HELLO_WORLD;
+    }
+
+
+}
--- src/test/java/dev/example/AppTest.java
+++ src/test/java/dev/example/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package dev.example;
 
 import static org.junit.Assert.assertTrue;
 
