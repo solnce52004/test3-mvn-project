abea1bb7e733b1aacd5b6c576993b20e39aa4dd3
mashagrig <m-a-grigoreva@eljur.ru>
2021-09-13 16:57:30 +0300

    <3> add and correct iml

--- module_bl/src/main/java/robots/T2000.java
+++ module_bl/src/main/java/robots/T2000.java
@@ -1,8 +1,11 @@
 package robots;
 
 public class T2000 {
+
+    public static final String T_2000 = "T2000";
+
     public String getRobotName() {
-        return "T1000";
+        return T_2000;
     }
 
 }
